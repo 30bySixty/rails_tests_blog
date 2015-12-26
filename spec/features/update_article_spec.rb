34e1839f5bb0844d6065ba06b7fa90aa02a4810f
Tatyana Shestopalova <tshestopalova@gmail.com>
2015-12-26 07:20:44 -0600

    Implement Update Article

---
+++ spec/features/update_article_spec.rb
@@ -0,0 +1,39 @@
+require "rails_helper"
+
+RSpec.feature "Edit an Article" do
+  
+  before do
+    @article = Article.create(title: "First Article", body: "Body of the first article.")
+  end
+  
+  scenario "A user updates an article" do
+    visit "/"
+    
+    click_link @article.title
+    click_link "Edit Article"
+    
+    fill_in "Title", with: "Updated article"
+    fill_in "Body", with: "Updated body of article"
+    
+    click_button "Update Article"
+    
+    expect(page).to have_content("Article has been updated.")
+    #want user to stay on article page once article has been updated
+    expect(page.current_path).to eq(article_path(@article))
+  end
+  
+  scenario "A user fails to update an article" do 
+    visit "/"
+    
+    click_link @article.title
+    click_link "Edit Article"
+    
+    fill_in "Title", with: ""
+    fill_in "Body", with: ""
+    
+    click_button "Update Article"
+    
+    expect(page).to have_content("Article has not been updated.")
+    expect(page.current_path).to eq(article_path(@article))
+  end
+end
