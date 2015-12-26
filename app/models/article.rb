5a65ff791c9a96eb42b2db021a63ad62e7ebcc95
Tatyana Shestopalova <tshestopalova@gmail.com>
2015-12-26 06:40:20 -0600

    Implement Read Articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ActiveRecord::Base
   validates :title, presence: {message: "cannot be blank."}
   validates :body, presence: {message: "cannot be blank."}
+  
+  default_scope { order(created_at: :desc)}
 end
