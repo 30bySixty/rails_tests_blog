e19bb6f09fc3db3f632fc615f8c3ae6025fa8b4c
Tatyana Shestopalova <tshestopalova@gmail.com>
2015-12-27 02:22:59 -0600

    update new user view

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ActiveRecord::Base
   has_secure_password
     
   validates_uniqueness_of :email
+  validates :password, :length => {minimum: 8, message: "Password must be at least 8 characters."}
+  
+  def minimum_password_length()
+    length = 8
+  end
+  
 end
